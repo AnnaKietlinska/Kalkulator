aef7dac61d77a5279149cef1724a5a9196316312
Anna Kietlińska <anna.jutyna.kietlinska.gmail.com>
2023-06-05 19:34:39 +0200

    initial commit

---
+++ src/main/java/org/example/Kalkulator.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Kalkulator {
+
+    public int add(int x, int y){
+        return x + y;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Kalkulator kalkulator = new Kalkulator();
+        System.out.println(kalkulator.add(  4, 7));
+    }
+}
