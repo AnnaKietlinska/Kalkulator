7e09c3dd4129205dae7e797f512e5d382f4a1713
Anna Kietlińska <anna.jutyna.kietlinska.gmail.com>
2023-06-05 20:02:34 +0200

    dodawanie metody substraction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Kalkulator kalkulator = new Kalkulator();
-        System.out.println(kalkulator.add(  4, 7));
+        System.out.println(kalkulator.add(4, 7));
         System.out.println(kalkulator.sub(4, 5.5));
     }
 }
