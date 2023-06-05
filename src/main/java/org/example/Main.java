f31e8124089fd885f5dad0448b33b04d2950c346
Anna Kietlińska <anna.jutyna.kietlinska.gmail.com>
2023-06-05 20:38:25 +0200

    dodawanie klasy Bank i dodoanie funkcji wplata

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Kalkulator kalkulator = new Kalkulator();
-        System.out.println(kalkulator.add(4, 7));
-        System.out.println(kalkulator.sub(4, 5.5));
+        Bank bank = new Bank();
+        bank.depozyt(1000);
+        System.out.println(bank.depozyt(1000));
     }
 }
